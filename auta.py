81384831bb07af55fd8546a0163d29cbafd0fe47
Maksymilian Nowak <maksiu102@wp.pl>
2023-05-18 00:18:39 +0200

    Add auta.py

--- auta.py
+++ auta.py
@@ -7,4 +7,11 @@ print(my_car)
 class Car:
     def __init__(self):
        pass
-          
+class Car:
+    def __init__(self,make,model_name,top_speed,color):
+        self.make = make
+        self.model_name = model_name
+        self.top_speed = top_speed
+        self.color= color
+mustang = Car(make="Ford",model_name="Mustang",top_speed=250,color="Yellow")
+print(mustang.color)    
